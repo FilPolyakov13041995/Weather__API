0858b4c59872f65d0d9dbbdf43221dcba816ab3a
FilPolyakov13041995 <Filipp-p@mail.ru>
2022-12-07 15:54:50 +0300

    TS

--- dist/weather.js
+++ dist/weather.js
@@ -1,27 +1,20 @@
-
 const urlWeather = 'https://api.openweathermap.org/data/2.5/weather?id=625143&appid=438826d17bf3b8ee4e5d95a8f1d1f416';
-
 const btn = document.querySelector('button');
 const cityName = document.querySelector('.city__name');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.description');
 const windSpeed = document.querySelector('.windSpeed');
-
-
 function weatherRequest(method, url) {
-    return fetch(urlWeather).then(response => {
+    return fetch(url).then(response => {
         return response.json();
     });
 }
-
 btn.addEventListener('click', () => {
     weatherRequest('GET', urlWeather)
-    .then(data => {
-        console.log(data);
+        .then(data => {
         cityName.textContent = data.name;
         temperature.innerHTML = Math.floor(data.main.temp - 273) + '&deg;';
         weatherDescription.textContent = data.weather[0].description;
         windSpeed.textContent = data.wind.speed;
-    })
-    .catch(err => console.log(err));
+    });
 });
